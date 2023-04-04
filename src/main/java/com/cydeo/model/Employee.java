4a5b29edea62b1aef3483deed9accf4f5fe10f1d
Baturay Gok <baturay.gok@gmail.com>
2023-04-04 09:43:49 -0500

    Started adding validations

--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -1,5 +1,9 @@
 package com.cydeo.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -10,6 +14,16 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class Employee {
 
+//    @NotNull  --> Field shouldn't be null
+//    @NotEmpty --> Field shouldn't be ""
+//    @NotBlank --> Field shouldn't be "     "
+
+//    @NotNull -> @NotNull
+//    @NotEmpty -> @NotNull + @NotEmpty
+//    @NotBlank -> @NotNull + @NotEmpty + @NotBlank
+
+    @NotBlank
+    @Size(max = 12, min = 2)
     private String firstName;
     private String lastName;
     // Thymeleaf accepts yyy-MM-dd, but LocalDate accepts a different format
